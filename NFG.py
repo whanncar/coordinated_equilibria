2f66a5a0b4ca73cf62ccbf309a81ab9aaac1edd0
Wade Hann-Caruthers <whanncar@gmail.com>
2019-10-09 13:41:50 -0700

    Added string method and test for NFG

--- NFG.py
+++ NFG.py
@@ -1,15 +1,30 @@
 class NFG:
 
-  def __init__(self, players, actions, states, payoffs):
-    self.players = players
-    self.actions = actions # dict: player -> list of actions
-    self.states = states
-    self.payoffs = payoffs # nested dict: state -> action of player 1 -> ... -> action of player n -> dict from players to payoffs
+	def __init__(self, players, actions, states, payoffs):
+		self.players = players
+		self.actions = actions # dict: player -> list of actions
+		self.states = states
+		self.payoffs = payoffs # nested dict: state -> action of player 1 -> ... -> action of player n -> dict from players to payoffs
 
-  def get_payoffs(self, ap, state):
-    result = self.payoffs
-    result = result[state]
-    for i in range(len(self.players)):
-      result = result[ap[self.players[i]]]
-    return result
+	def get_payoffs(self, ap, state):
+		result = self.payoffs
+		result = result[state]
+		for i in range(len(self.players)):
+			result = result[ap[self.players[i]]]
+		return result
 
+
+	def __str__(self):
+		result = ""
+		result = result + "\nStates\n\n"
+		for s in self.states:
+			result = result + "  " + s + "\n"
+		result = result + "\n\n"
+		result = result + "Players and actions\n\n"
+		for p in self.players:
+			result = result + "  " + p + "\n"
+			for a in self.actions[p]:
+				result = result + "    " + a + "\n"
+		result = result + "\n\n"
+		result = result + str(self.payoffs)
+		return result
