3d3ae448ed839b201e5da2b9b903a0cf7f04bde6
Wade Hann-Caruthers <whanncar@gmail.com>
2020-02-03 11:47:56 -0800

    Added a method for checking which actions are in support of some unequilibrium

--- code/eq_calculators/calculator.py
+++ code/eq_calculators/calculator.py
@@ -1,6 +1,6 @@
 
 import LP_wrapper
-
+import unLP
 
 def run_LP(objective, lp_matrix):
 	# Make zero vector
@@ -10,3 +10,27 @@ def run_LP(objective, lp_matrix):
 		zeros.append(0)
 	# Run LP
 	return LP_wrapper.max_dist_lp_container(objective, lp_matrix, zeros, [], [])
+
+
+
+
+
+def get_supported_actions(game):
+	# Get unplans and LP matrix
+	LP_data = unLP.prepare_LP_data(game)
+	unplans = LP_data[0]
+	LP_matrix = LP_data[1]
+	# Get action indicators
+	action_indicators = unLP.get_action_indicators(game, unplans)
+	# Calculate supported actions
+	result = {}
+	players = game.players
+	actions = game.actions
+	for p in players:
+		result[p] = []
+		for a in actions[p]:
+			max_prob_for_a = run_LP(action_indicators[p][a], LP_matrix)
+			# DEFINING TOLERANCE FOR SUPPORT HERE
+			if max_prob_for_a > .000001:
+				result[p].append(a)
+	return result
